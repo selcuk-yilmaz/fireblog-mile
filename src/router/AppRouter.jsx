7d887cde381247bb79a4340952b8543c4b4f8916
selcuklu <yilmax4@gmail.com>
2022-08-04 21:45:26 +0300

    ok

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,5 +1,11 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useContext } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Dashboard from "../pages/Dashboard";
 import Login from "../pages/Login";
@@ -7,8 +13,14 @@ import Details from "../pages/Details";
 import Profile from "../pages/Profile";
 import NewBlog from "../pages/NewBlog";
 import Register from "../pages/Register";
+import { AuthContext } from "../contexts/AuthContext";
 
 const AppRouter = () => {
+  const { auth } = useContext(AuthContext);
+  function PrivateRouter() {
+    return auth ? <Outlet /> : <Navigate to="/login" replace />;
+  }
+
   return (
     <>
       <BrowserRouter>
@@ -16,7 +28,10 @@ const AppRouter = () => {
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="login" element={<Login />} />
-          <Route path="dashboard/:id" element={<Details />} />
+          {/* <Route path="dashboard/:id" element={<Details />} /> */}
+          <Route path="details" element={<PrivateRouter />}>
+            <Route path="" element={<Details />} />
+          </Route>
           <Route path="profile" element={<Profile />} />
           <Route path="newBlog" element={<NewBlog />} />
           <Route path="register" element={<Register />} />
